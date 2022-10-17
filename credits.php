2af14d3618f0fc7e7ec343dd4ef7125adbcaa172
OfficialB <reconispog@gmail.com>
2022-10-17 14:30:36 -0700

    Update credits.php

--- credits.php
+++ credits.php
@@ -1,5 +1,6 @@
 <?php include ("includes.php"); ?>
 <div class="container">
     <h3>Blog software by BlogBB Team.</h3>
-    <p>Scott (piss#6669) - i did everything</p>
+    <p>Scott (left discord) - Creator of the original BlogBB.</p>
+    <p>B (Official B#0001) - Maintaining the project.</p>
 </div>
